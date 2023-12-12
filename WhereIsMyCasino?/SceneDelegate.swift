7232ae0f7aa8ff36d0e90fee110839893e669725
Vyacheslav <slava.orlob@gmail.com>
2023-12-12 11:58:11 +0300

    Added WKWebView

--- WhereIsMyCasino?/SceneDelegate.swift
+++ WhereIsMyCasino?/SceneDelegate.swift
@@ -15,31 +15,35 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let loadingViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Loading") as! LoadingViewController
-        let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
-        
+//        let loadingViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Loading") as! LoadingViewController
+//        let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+//        
         let window = UIWindow(windowScene: windowScene)
-        
-        if UserDefaults.standard.bool(forKey: "loadingViewControllerShown") {
-            
-            window.rootViewController = tabBarController
-            window.makeKeyAndVisible()
-            
-        }
-        
-        else {
-            
-            window.rootViewController = loadingViewController
-            window.makeKeyAndVisible()
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
-                window.rootViewController = tabBarController
-                window.makeKeyAndVisible()
-                
-            }
-        }
+//        
+//        if UserDefaults.standard.bool(forKey: "loadingViewControllerShown") {
+//            
+//            window.rootViewController = tabBarController
+//            window.makeKeyAndVisible()
+//            
+//        }
+//        
+//        else {
+//            
+//            window.rootViewController = loadingViewController
+//            window.makeKeyAndVisible()
+//            
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
+//                window.rootViewController = tabBarController
+//                window.makeKeyAndVisible()
+//                
+//            }
+//        }
+//        
+        window.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "WebView")
         
         self.window = window
+        
+        window.makeKeyAndVisible()
     }
 }
 
--- WhereIsMyCasino?/WebView/WebViewController.swift
+++ WhereIsMyCasino?/WebView/WebViewController.swift
@@ -1,34 +1,80 @@
-//
-//  WebViewController.swift
-//  1
-//
-//  Created by Слава Орлов on 28.11.2023.
-//
-
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController {
-
-    @IBOutlet weak var WebView: WKWebView!
+class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
-    var url: URL?
+    var webView: WKWebView!
+    
+    var domains = ["vk.com" , "ok.ru" , "oauth.yandex.ru" , "mail.ru"]
+            
+    let lastURLKey = "lastVisitedURLKey"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.websiteDataStore = WKWebsiteDataStore.default()
         
-    }
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
         
+        webView.allowsBackForwardNavigationGestures = true
+        webView.configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
 
-    /*
-    // MARK: - Navigation
+        view = webView
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let savedURLString = UserDefaults.standard.string(forKey: lastURLKey)
+        let lastURL = savedURLString.flatMap { URL(string: $0) }
+        let request = URLRequest(url: lastURL ?? URL(string: "https://1wayrw.xyz/")!)
+        webView.load(request)
     }
-    */
 
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        if let urlString = webView.url?.absoluteString {
+            UserDefaults.standard.set(urlString, forKey: lastURLKey)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let cookies = HTTPCookieStorage.shared.cookies
+        if let cookies = cookies {
+            for cookie in cookies {
+                if cookie.domain.contains("ourauthpoint666.com") || cookie.domain.contains("oauth.yandex.ru") || cookie.domain.contains("vk.ru") || cookie.domain.contains("ok.ru") || cookie.domain.contains("tg.ru") || cookie.domain.contains("oauth.yandex.ru") || cookie.domain.contains("steamcommunity.com") {
+                    
+                    UserDefaults.standard.removeObject(forKey: lastURLKey)
+                    HTTPCookieStorage.shared.deleteCookie(cookie)
+                    print("deleted")
+                    
+                    let initialURL = URL(string: "https://1wayrw.xyz/")!
+                    let request = URLRequest(url: initialURL)
+                    
+                    print("Cookie Domain - \(cookies.map({ $0.domain })) --------- \n\n\n")
+                    print("Cookie Name- \(cookies.map({ $0.name })) ---------- \n\n\n")
+                    
+                    webView.load(request)
+                }
+            }
+            print("Cookie Domain - \(cookies.map({ $0.domain }))")
+            print("Cookie Name- \(cookies.map({ $0.name }))")
+        }
+    }
+    
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated {
+            if let url = navigationAction.request.url {
+                webView.load(URLRequest(url: url))
+                decisionHandler(.cancel)
+                return
+            }
+        }
+        decisionHandler(.allow)
+    }
+    
+
+    // WKNavigationDelegate methods и другие
 }
