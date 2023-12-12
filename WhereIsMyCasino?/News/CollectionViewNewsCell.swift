a2ad90ae388b5537150526f5ed499c0a4b1ddd67
Vyacheslav <slava.orlob@gmail.com>
2023-12-12 12:07:55 +0300

    Fix cell in new

---
+++ WhereIsMyCasino?/News/CollectionViewNewsCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewNewsCell.swift
+//  WhereIsMyCasino?
+//
+//  Created by Слава Орлов on 12.12.2023.
+//
+
+import UIKit
+
+class CollectionViewNewsCell: UICollectionViewCell {
+    
+}
