5d45f7cd0063dab2ab195c27abc2a7469f00a75a
Vyacheslav <slava.orlob@gmail.com>
2023-12-12 12:08:46 +0300

    UpdateUI

--- WhereIsMyCasino?/News/CollectionViewNewsCell.swift
+++ WhereIsMyCasino?/News/CollectionViewNewsCell.swift
@@ -9,4 +9,23 @@ import UIKit
 
 class CollectionViewNewsCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var hotNewLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.heightAnchor.constraint(equalToConstant: 205).isActive = true
+        self.widthAnchor.constraint(equalToConstant: 425).isActive = true
+        
+        imageView.layer.cornerRadius = 8
+        imageView.clipsToBounds = true
+        
+    }
 }
+    
+    
+
